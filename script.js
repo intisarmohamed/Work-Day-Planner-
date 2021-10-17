789bfdd58c02a0a4ea3606e69f9750771f28323a
Intisar Mohamed <Intisarm48@gmail.com>
2021-10-17 03:03:44 +0100

    pseudocode  for js

---
+++ script.js
@@ -0,0 +1,16 @@
+// create variables
+
+//functions
+//create a function which shows current day at the top of page
+// use moment.js
+
+//create a time block which is color coded depending on past, present, future
+// create a function with an if statement
+
+//create a function when click on time block and being able to enter event
+
+// save button is clicked, then data is saved to localStorage
+//create an event listener for save button which saves to localStorage
+
+//page is refreshed and all the events are cleared
+//create an event listener for refresh button which clears localStorage
