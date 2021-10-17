407a5cf02d5f4122919c6d6063d84ef197e7a873
Intisar Mohamed <Intisarm48@gmail.com>
2021-10-17 17:57:25 +0100

    added cuurent day

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -3,6 +3,10 @@
 //functions
 //create a function which shows current day at the top of page
 // use moment.js
+// get current day
+// render current day
+$("#currentDay").text(moment().format("dddd MMMM Do YYYY"));
+console.log("#currentDay");
 
 //create a time block which is color coded depending on past, present, future
 // create a function with an if statement
